36bdb9bb9fee027374e2a45533448759cba8963f
Yogita <129670547+yogita-sonar@users.noreply.github.com>
2023-04-21 14:00:16 +0530

    SumNnum
    Sum of First 20 Natural Number

---
+++ SumNnum.java
@@ -0,0 +1,17 @@
+
+public class SumNnum {
+
+	
+		public static void main(String[] args)   
+		{  
+		int i, num = 20, sum = 0;  
+		
+		for(i = 1; i <= num; ++i)  
+		{  
+	
+		sum = sum + i;  
+		}  
+	
+		System.out.println("Sum of First 20 Natural Numbers is = " + sum);  
+		}
+}
